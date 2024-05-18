d0112ef8cb35b4b9aad465f1af3ced5a37045231
Teo <92630256+StaticVoidTeo@users.noreply.github.com>
2024-05-18 19:50:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 const select = document.querySelector(".options select");
+const socialMedia = document.querySelector(".socialMedia");
+const dotDot = document.querySelector(".showMedia");
+const fb = document.querySelector("#fb");
+const insta = document.querySelector("#insta");
+
+let socialShown = false;
+
+fb.addEventListener("click", () => alert("This would take you to the cafe's facebook page."));
+insta.addEventListener("click", () => alert("This would take you to the cafe's instagram page."));
+
+dotDot.addEventListener("click", () => {
+    if(socialShown){
+        socialShown = false;
+        fb.style = "display:none;";
+        insta.style = "display:none;";
+    }
+    else{
+        socialShown = true;
+        fb.style = "display:block;";
+        insta.style = "display:block;";
+    }
+})
 
 select.addEventListener("change", () => {
     document.querySelectorAll(".card").forEach(el => {
