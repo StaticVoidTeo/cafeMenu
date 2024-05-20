ea464fd302f24a1a1c6ba03007d24ee7bebc2f7c
Teo <92630256+StaticVoidTeo@users.noreply.github.com>
2024-05-20 17:36:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ let socialShown = false;
 fb.addEventListener("click", () => alert("This would take you to the cafe's facebook page."));
 insta.addEventListener("click", () => alert("This would take you to the cafe's instagram page."));
 
+window.addEventListener("scroll", () => {
+    socialShown = false;
+    fb.style = "display:none;";
+    insta.style = "display:none;";
+})
+
 dotDot.addEventListener("click", () => {
     if(socialShown){
         socialShown = false;
